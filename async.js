efe1fb2fc534d686c89d10e9af03315ac7f35fd7
Taracheva Ekaterina <katyataracheva@gmail.com>
2017-11-30 02:04:33 +0500

    first version

--- async.js
+++ async.js
@@ -7,7 +7,36 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns Promise
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise((resolve, reject) => {
+        let results = [];
+        let numberOfCurrentJobs = 0;
+        let jobIndex = 0;
+        if (!jobs.length) {
+            resolve([]);
+        }
+        while (numberOfCurrentJobs < parallelNum) {
+            numberOfCurrentJobs++;
+            runJob(jobIndex);
+        }
+        
+        function runJob(currentJobIndex){
+            jobs[currentJobIndex]()
+                .then(result => processingResult(result, currentJobIndex))
+                .catch(result => processingResult(result, currentJobIndex));
+            jobIndex++;
+        } 
+
+        function processingResult(result, currentJobIndex){
+            results[currentJobIndex] = result;
+            if (jobIndex < jobs.length) {
+                runJob(jobIndex);  
+            }
+            if (results.length === jobs.length) {
+                resolve(results);
+            }
+        }
+    });
 }
