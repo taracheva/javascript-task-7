d83881b08628c5d793f373feac789d6c20d70869
Taracheva Ekaterina <katyataracheva@gmail.com>
2017-12-07 00:20:48 +0500

    after fix

--- async.js
+++ async.js
@@ -21,21 +21,22 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
             numberOfCurrentJobs++;
             runJob(jobIndex);
         }
-        
-        function runJob(currentJobIndex){
-            let errorTimeout = new Promise(resolve => {
-                setTimeout(resolve, timeout, new Error(`Promise timeout`));
-            });
-            Promise.race([jobs[currentJobIndex](), errorTimeout])
-                .then(result => processingResult(result, currentJobIndex))
-                .catch(result => processingResult(result, currentJobIndex));
-            jobIndex++;
-        } 
 
-        function processingResult(result, currentJobIndex){
+        function runJob(currentJobIndex) {
+            let errorTimeout = new Promise(resolve => {
+                setTimeout(resolve, timeout, new Error('Promise timeout'));
+            });
+            let saveResult = result => processingResult(result, currentJobIndex);
+            Promise.race([jobs[currentJobIndex](), errorTimeout])
+                .then(saveResult)
+                .catch(saveResult);
+            jobIndex++;
+        }
+
+        function processingResult(result, currentJobIndex) {
             results[currentJobIndex] = result;
             if (jobIndex < jobs.length) {
-                runJob(jobIndex);  
+                runJob(jobIndex);
             }
             if (results.length === jobs.length) {
                 resolve(results);
